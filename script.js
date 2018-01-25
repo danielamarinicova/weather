c6da4c8445480f624a6adc6fa781f4cfe93b3049
Daniela Marinicova <marinicova.d@gmail.com>
2018-01-25 09:38:59 +0100

    25.1.2018

--- script.js
+++ script.js
@@ -18,12 +18,48 @@ $(document).ready(function(){
 				data:{ format: 'json'	},
 		
 			error : function(){
-				//vypis chyby
+				$("#main").html("Error connection.")
 			},
 			dataType: 'json',
 			success : function(data){
-				console.log("temp:"+data.main.temp);
-				console.log("desc:"+data.weather[0].description);
+		
+				$("#main").empty;
+				var table=$("<table/>");
+				
+				var tr=getLine("City:",city);
+				table.append(tr);
+				var tr=getLine("Temperature:", data.main.temp-273.15,1);
+				table.append(tr);
+				var tr=getLine("Humidity:", data.main.humidity );
+				table.append(tr);
+				var tr=getLine("Description:", data.main.description );
+				table.append(tr);
+				var tr=getLine("Pressure:", data.main.pressure );
+				table.append(tr);
+
+				if($("#details").is(':checked')){
+				var tr=getLine("Sunrise:", data.main.sunrise);
+				table.append(tr);
+				var tr=getLine("Sunset:", data.main.sunset);
+				table.append(tr);
+				var tr=getLine("Wind Speed:", data.main.wind);
+				table.append(tr);
+				var tr=getLine("Min:", data.main.temp_min);
+				table.append(tr);
+				var tr=getLine("Max:", data.main.temp_max);
+				table.append(tr);
+				var tr=getLine("Visibility:", data.main.visibility);
+				table.append(tr);
+		
+				}
+
+
+
+				
+
+
+				$("#main").append(table);
+
 			},
 
 			type: 'GET'
@@ -33,4 +69,17 @@ $(document).ready(function(){
 	}
 
 		});
+function getLine(data1,data2){
+
+	var tr=$("<tr/>");
+				var td1 =$("<td/>");
+				$(td1).append(data1);
+				var td2 =$("<td/>");
+				$(td2).append(data2);
+				tr.append(td1);
+				tr.append(td2);
+
+}
+
+
 });
